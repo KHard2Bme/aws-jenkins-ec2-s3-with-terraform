50410862801851a58e5d6d0395dbf0bdd3e51646
Kevin Harding, MIS <146769989+KHard2Bme@users.noreply.github.com>
2025-09-02 01:37:43 -0400

    up

--- jenkins_bootstrap.sh
+++ jenkins_bootstrap.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-set -e
 
-# Update system packages
+# Update all system packages to the latest version
+sudo yum update -y
+
+# Add Jenkins repository file 
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+# Import the Jenkins repository key 
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+# Upgrade all packages again
 sudo yum update -y
 
 # Install Java 17 (Amazon Linux 2023 default supports java-17)
-sudo dnf install -y java-17-amazon-corretto
-
-# Add Jenkins repo and key
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo dnf install java-17-amazon-corretto -y
 
 # Install Jenkins
-sudo dnf install -y jenkins
+sudo yum install jenkins -y
 
-# Enable and start Jenkins
+# Enable and start Jenkins on system boot
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
--- main.tf
+++ main.tf
@@ -62,9 +62,9 @@ resource "aws_instance" "jenkins" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
-  user_data              = file ("jenkins_bootstrap.sh")
+  user_data              = file("var.user_data_file")
 
-  
+
   tags = {
     Name = "Jenkins-Server-ec2"
   }
