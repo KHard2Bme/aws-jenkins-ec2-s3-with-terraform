89a2f4c7575ea668796ce6c0a961e2968ba53d86
Kevin Harding, MIS <146769989+KHard2Bme@users.noreply.github.com>
2025-09-02 00:29:50 -0400

    up

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_instance" "jenkins" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
-  user_data              = file("jenkins_bootstrap.sh")
+  user_data              = file ("jenkins_bootstrap.sh")
 
   
   tags = {
